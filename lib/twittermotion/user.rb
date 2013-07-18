640b01552172037dadd5622f3d32e63c283a561e
AndrewGertig <andrew.gertig@gmail.com>
2013-07-17 23:06:45 -0400

    add users method

--- lib/twittermotion/user.rb
+++ lib/twittermotion/user.rb
@@ -37,20 +37,50 @@ module Twitter
     end
 
     # Returns up to 5,000 friend ids, have to implement Cursors to access multiple pages of results
-    def friend_ids(options = {}, &block)
-      url = NSURL.URLWithString("http://api.twitter.com/1.1/friends/ids.json")
-      request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
-      request.account = self.ac_account
-      request.performRequestWithHandler(lambda {|response_data, url_response, error|
-        if !response_data
-          block.call(nil, error)
-        else
-          block.call(BubbleWrap::JSON.parse(response_data), nil)
-        end
-      })
-    end
+    # def friend_ids(options = {}, &block)
+    #   url = NSURL.URLWithString("http://api.twitter.com/1.1/friends/ids.json")
+    #   request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
+    #   request.account = self.ac_account
+    #   request.performRequestWithHandler(lambda {|response_data, url_response, error|
+    #     if !response_data
+    #       block.call(nil, error)
+    #     else
+    #       block.call(BubbleWrap::JSON.parse(response_data), nil)
+    #     end
+    #   })
+    # end
 
-    def lock_friend_ids(options = {})
+        # Returns up to 5,000 follower ids, have to implement Cursors to access multiple pages of results
+    # def follower_ids(options = {}, &block)
+    #   url = NSURL.URLWithString("http://api.twitter.com/1.1/followers/ids.json")
+    #   request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
+    #   request.account = self.ac_account
+    #   request.performRequestWithHandler(lambda {|response_data, url_response, error|
+    #     if !response_data
+    #       block.call(nil, error)
+    #     else
+    #       block.call(BubbleWrap::JSON.parse(response_data), nil)
+    #     end
+    #   })
+    # end
+
+    # def users(options = {}, &block)
+    #   url = NSURL.URLWithString("http://api.twitter.com/1.1/users/lookup.json")
+    #   request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
+    #   request.account = self.ac_account
+    #   request.performRequestWithHandler(lambda {|response_data, url_response, error|
+    #     if !response_data
+    #       block.call(nil, error)
+    #     else
+    #       block.call(BubbleWrap::JSON.parse(response_data), nil)
+    #     end
+    #   })
+    # end
+
+
+    # This method will lock the thread it is called in because it is Synchronous
+    # Returns up to 5,000 friend ids, have to implement Cursors to access multiple pages of results
+    def friend_ids(options = {})
       url = NSURL.URLWithString("http://api.twitter.com/1.1/friends/ids.json")
       request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
       request.account = self.ac_account
@@ -61,32 +91,34 @@ module Twitter
       return BubbleWrap::JSON.parse(ns_data)
     end
 
+    # This method will lock the thread it is called in because it is Synchronous
     # Returns up to 5,000 follower ids, have to implement Cursors to access multiple pages of results
-    def follower_ids(options = {}, &block)
+    def follower_ids(options = {})
       url = NSURL.URLWithString("http://api.twitter.com/1.1/followers/ids.json")
       request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
       request.account = self.ac_account
-      request.performRequestWithHandler(lambda {|response_data, url_response, error|
-        if !response_data
-          block.call(nil, error)
-        else
-          block.call(BubbleWrap::JSON.parse(response_data), nil)
-        end
-      })
+      ns_url_request = request.signedURLRequest
+      ns_url_response_ptr = Pointer.new(:object)
+      error_ptr = Pointer.new(:object)
+      ns_data = NSURLConnection.sendSynchronousRequest(ns_url_request, returningResponse:ns_url_response_ptr, error: error_ptr)
+      return BubbleWrap::JSON.parse(ns_data)
     end
 
-    def users(options = {}, &block)
+    # This method will lock the thread it is called in because it is Synchronous
+    # Returns up to 100 hydrated users can call it 180 times in a 15 minute span
+    def users(*args)
+      options = { user_id: args }
       url = NSURL.URLWithString("http://api.twitter.com/1.1/users/lookup.json")
       request = TWRequest.alloc.initWithURL(url, parameters:options, requestMethod:TWRequestMethodGET)
       request.account = self.ac_account
-      request.performRequestWithHandler(lambda {|response_data, url_response, error|
-        if !response_data
-          block.call(nil, error)
-        else
-          block.call(BubbleWrap::JSON.parse(response_data), nil)
-        end
-      })
+      ns_url_request = request.signedURLRequest
+      ns_url_response_ptr = Pointer.new(:object)
+      error_ptr = Pointer.new(:object)
+      ns_data = NSURLConnection.sendSynchronousRequest(ns_url_request, returningResponse:ns_url_response_ptr, error: error_ptr)
+      return BubbleWrap::JSON.parse(ns_data)
     end
+
+
 
   end
 end
